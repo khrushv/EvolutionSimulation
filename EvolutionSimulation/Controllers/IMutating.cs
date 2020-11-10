3a725c0df1c12a708cc21eebed91dbded4f45fa7
Khrush <arrmadon@gmail.com>
2020-11-10 11:04:15 +0300

    Interface for bacteria

---
+++ EvolutionSimulation/Controllers/IMutating.cs
@@ -0,0 +1,19 @@
+﻿interface IMutating
+{
+	public void Move(Tuple<float, float> centerFood, Tuple<float, float> red
+			Tuple<float, float> green, Tuple<float, float> blue, float time);
+
+	public static void Eat<T>(T object);
+
+	public void Init(Genome g)
+
+	public Tuple<float, float> getCoordinates();
+
+	private void setCoordinates(Tuple<int, int> coord);
+
+	public Color getColor();
+
+	private void setColor(Color c);
+
+	public float GetDamage(IMutable source);
+}
